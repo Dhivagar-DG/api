1b7d00d92d4f55b0df36afde91f2051bafab0ea1
Dhivagar M <95082121+Dhivagar-DG@users.noreply.github.com>
2022-07-23 21:25:06 +0530

    Update crud_API_xhr.js

--- crud_API_xhr.js
+++ crud_API_xhr.js
@@ -5,7 +5,7 @@ const url = "https://jsonplaceholder.typicode.com/"
 const xhr = new XMLHttpRequest();
 
 // Append user details to interface
-function print(response , upadteFlag =null){
+function showUserDetails(response , upadteFlag =null){
     if (upadteFlag == null) {
         for(value of response.slice(0,5)){
             console.log(value.name, value.username, value.email);
@@ -33,7 +33,7 @@ function print(response , upadteFlag =null){
 $('#fetchBtn').on("click", fetch);
 
 
-// POST METHOD
+// Create New Post Method
 $('#postBtn').on('click', function(){
     let _name = prompt("Enter your name..") ?? "";
     let username = prompt("Enter username..") ?? "";
@@ -75,10 +75,9 @@ function postUpdate(t_this){
 function fetch(){
     xhr.onload = ()=>{
         if(xhr.status >=200 && xhr.status <=299){
-            print(JSON.parse(xhr.response));
-            // console.log(JSON.parse(xhr.response));
+            showUserDetails(JSON.parse(xhr.response));
         } else{
-            $('.disply').append(`<p>error : somethig  went wrong</p>`);
+            alert("error : somethig  went wrong");
         }
     };
     xhr.open("GET", `${url}users`);
@@ -88,13 +87,12 @@ function fetch(){
 
 // Post request
 function newPost(postData){
-    console.log(postData);
     xhr.onload = ()=>{
         if(xhr.status >=200 && xhr.status <=299){
-            print([JSON.parse(xhr.response)]);
+            showUserDetails([JSON.parse(xhr.response)]);
             alert("Created Successfully.")
         }else{
-            $('.disply').append(`<p>error : somethig  went wrong</p>`); 
+            alert("error : somethig  went wrong"); 
         }
     }
 
@@ -109,7 +107,7 @@ function updateRequest(data, id){
 
     xhr.onload = ()=>{
         if(xhr.status >= 200 && xhr.status <=299){
-            print(JSON.parse(xhr.response), true);
+            showUserDetails(JSON.parse(xhr.response), true);
             alert("Updated Successfully");
         } else{
             alert("Something went wrong");
@@ -125,7 +123,6 @@ function updateRequest(data, id){
 // Delete Request
 function postRemove(t_this){
     let deleteId = $(t_this).closest('div').attr('id')
-    console.log(deleteId);
     xhr.onload = ()=>{
         if(xhr.status >= 200 && xhr.status <= 299){
             $('#'+deleteId).remove();
